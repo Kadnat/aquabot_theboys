429918f996e859024b569642c12e97be59a70b92
Nathanaël BLAVO-BALLARIN <blavonathanael@gmail.com>
2023-11-15 00:02:15 +0100

    Version du 14/11.
    Signed-off-by: Nathanaël BLAVO-BALLARIN <blavonathanael@gmail.com>

--- aquabot_theboys/cmd_motors.py
+++ aquabot_theboys/cmd_motors.py
@@ -15,11 +15,9 @@ class MyCmdMotors(Node):
         self.pub_pos = self.create_publisher(Float64, '/wamv/thrusters/main/pos', 10)
         # vitesse du propulseur
         self.pub_thrust = self.create_publisher(Float64, '/wamv/thrusters/main/thrust', 10)
-        timerthrust_period = 0.1  # Période du timer en secondes
-        timerpos_period = 1  # Période du timer en secondes
+        timer_period = 0.1  # Période du timer en secondes
         # Appel la fonction pour la cmd moteurs toutes les 100ms
-        self.timerthrust = self.create_timer(timerthrust_period, self.timerthrust_callback)
-        self.timerpos = self.create_timer(timerpos_period, self.timerpos_callback)
+        self.timerthrust = self.create_timer(timer_period, self.timer_callback)
         self.create_subscription(Float64MultiArray, '/cmd_motors', self.cmd_motors_callback, 10)
         self.pos = 0.0
         self.thrust = 0.0
@@ -27,66 +25,78 @@ class MyCmdMotors(Node):
         self.dist_cmd_motors=0.0
         self.turn_in_progress=False
         self.first_turn=True
-        #self.first_in = 0
-        self.dist_ancienne = 10000
+        self.first_in = 0
+        self.stop_turn=False
+        self.is_accelerating=False
 
-    def timerpos_callback(self):
-        self.turn_in_progress=True
-        if(self.first_turn==True):
-            self.thrust=0
-            self.first_turn=False
-        if ((self.angle_cmd_motors >= 0.2) & (self.angle_cmd_motors <= math.pi/2)):
-            self.pos = math.pi/8
-            self.thrust = 1000
-        elif ((self.angle_cmd_motors >= math.pi/2) & (self.angle_cmd_motors <= math.pi)):
-            self.pos = math.pi/4
-            self.thrust = 1000
-        elif ((self.angle_cmd_motors >= -math.pi) & (self.angle_cmd_motors <= -math.pi/2)):
-            self.pos = -math.pi/4
-            self.thrust = 1000
-        elif ((self.angle_cmd_motors >= -math.pi/2) & (self.angle_cmd_motors <= -0.2)):
-            self.pos = math.pi/8
-            self.thrust = 1000
-        elif ((self.angle_cmd_motors >= -0.2) & (self.angle_cmd_motors <= 0)):
-            self.pos = 0
-            self.thrust = 0
-            self.turn_in_progress=False
-            self.first_turn=True
-        elif ((self.angle_cmd_motors >= 0) & (self.angle_cmd_motors <= 0.2)):
-            self.pos = 0
-            self.thrust = 0
-            self.turn_in_progress=False
-            self.first_turn=True
+    def position_corrector(self):
 
-
-    def timerthrust_callback(self):
         msg_pos = Float64()
         msg_thrust = Float64()
-        cmd_dist = self.dist_cmd_motors
-        
-        if(self.turn_in_progress == False):
-            if(cmd_dist>=100.0):
-                self.pos=0.0
-                self.thrust=12000
-            else:
-                if(cmd_dist>=50):
-                    self.pos=0.0
-                    self.thrust=5000
-                else:
-                    if(cmd_dist>=20):
-                        self.pos=0.0
-                        self.thrust=1000
-                    else:
-                        if(cmd_dist>10):
-                            self.pos=0.0
-                            self.thrust=100
-                        else:
-                            if(cmd_dist<5):
-                                self.pos=0.0
-                                self.thrust=0.0
+
+        while (((self.angle_cmd_motors >= 0) & (self.angle_cmd_motors <= 0.2))|((self.angle_cmd_motors >= -0.2) & (self.angle_cmd_motors <= 0))):
+            # if ((self.angle_cmd_motors >= 0.2) & (self.angle_cmd_motors <= math.pi/2)):
+            #     self.pos = -math.pi/8
+            #     self.thrust = 500.0
+            # elif ((self.angle_cmd_motors >= math.pi/2) & (self.angle_cmd_motors <= math.pi)):
+            #     self.pos = -math.pi/4
+            #     self.thrust = 500.0
+            # elif ((self.angle_cmd_motors >= -math.pi) & (self.angle_cmd_motors <= -math.pi/2)):
+            #     self.pos = math.pi/4
+            #     self.thrust = 500.0
+            # elif ((self.angle_cmd_motors >= -math.pi/2) & (self.angle_cmd_motors <= -0.2)):
+                self.pos = math.pi/8
+                self.thrust = 250.0
+
+                msg_pos.data= self.pos
+                msg_thrust.data = self.thrust
+                # On publie les messages les messages
+                self.pub_pos.publish(msg_pos)
+                self.pub_thrust.publish(msg_thrust)
+
+
+
+    def motor_controller(self):
+        # cmd_dist = self.dist_cmd_motors
+        # if(self.is_accelerating==False):
+        #     if(cmd_dist>=100.0):
+        #         self.position_corrector()
+        #         self.acceleration(5000.0)
+        #     else:
+        #         if(cmd_dist>=50):
+        #             #self.decceleration()
+        #             self.position_corrector()
+        #             self.acceleration(5000.0)
+        #         else:
+        #             if(cmd_dist>=20):
+        #                 #self.decceleration()
+        #                 self.position_corrector()
+        #                 self.acceleration(1000.0)
+        #             else:
+        #                 if(cmd_dist>10):
+        #                     #self.decceleration()
+        #                     self.position_corrector()
+        #                     self.acceleration(100.0)
+        #                 else:
+        #                     if(cmd_dist<5):
+        #                         self.pos=0.0
+        #                         self.thrust=0.0
+
+        while (self.dist_cmd_motors<5):
             
-        else:
-            self.get_logger().info('Wait the USV turn')
+            if(((self.angle_cmd_motors >= 0) & (self.angle_cmd_motors <= 0.2))|((self.angle_cmd_motors >= -0.2) & (self.angle_cmd_motors <= 0))):
+                self.decceleration()
+                self.position_corrector()
+            else:
+                self.acceleration(5000.0)
+
+             
+
+    def timer_callback(self):
+        msg_pos = Float64()
+        msg_thrust = Float64()
+
+        self.motor_controller()
 
         msg_pos.data= self.pos
         msg_thrust.data = self.thrust
@@ -94,16 +104,52 @@ class MyCmdMotors(Node):
         self.pub_pos.publish(msg_pos)
         self.pub_thrust.publish(msg_thrust)
         # Affichage de la commande envoyé
-        self.get_logger().info('Envoi: pos = %f, thrust = %f' % (msg_pos.data, msg_thrust.data))                
+        self.get_logger().info('Envoi: pos = %f, thrust = %f' % (msg_pos.data, msg_thrust.data))  
 
                 
+    def acceleration(self, final_value):
+        # self.pos=0.0
+        # self.thrust=0.0
+        # msg_pos = Float64()
+        # msg_thrust = Float64()
+        # for i in range(3):
+        #     self.is_accelerating=True
+        #     self.thrust = self.thrust + final_value/3
+        #     msg_pos.data= self.pos
+        #     msg_thrust.data = self.thrust
+        #     self.pub_pos.publish(msg_pos)
+        #     self.pub_thrust.publish(msg_thrust)
+        #     self.get_logger().info('Accel: pos = %f, thrust = %f' % (msg_pos.data, msg_thrust.data))    
+        #     sleep(2)
+        # self.is_accelerating=False
 
+        self.pos=0.0
+        msg_pos = Float64()
+        msg_thrust = Float64()
+        self.is_accelerating=True
+        self.thrust = final_value
+        msg_pos.data= self.pos
+        msg_thrust.data = self.thrust
+        self.pub_pos.publish(msg_pos)
+        self.pub_thrust.publish(msg_thrust)
+        self.get_logger().info('Accel: pos = %f, thrust = %f' % (msg_pos.data, msg_thrust.data))    
+        #sleep(10)
+        self.is_accelerating=False
 
+    def decceleration(self):    
+        msg_pos = Float64()
+        msg_thrust = Float64()
+        self.thrust = 0.0
+        msg_pos.data= self.pos
+        msg_thrust.data = self.thrust
+        self.pub_pos.publish(msg_pos)
+        self.pub_thrust.publish(msg_thrust)  
+        sleep(5)
             
 
     def cmd_motors_callback(self, msg):
-        #self.angle_cmd_motors, self.dist_cmd_motors = msg.data
-        self.get_logger().info('Received: angle = %f, distance = %f' % (self.angle_cmd_motors, self.dist_cmd_motors))
+        self.angle_cmd_motors, self.dist_cmd_motors = msg.data
+        #self.get_logger().info('Received: angle = %f, distance = %f' % (self.angle_cmd_motors, self.dist_cmd_motors))
 
 
 def main(args=None):
