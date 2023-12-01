78b74c434f6658725f52a29d44b68c96f155e923
Nathanaël BLAVO-BALLARIN <blavonathanael@gmail.com>
2023-12-01 23:19:57 +0100

    commentaires.
    Signed-off-by: Nathanaël BLAVO-BALLARIN <blavonathanael@gmail.com>

--- aquabot_theboys/cmd_motors.py
+++ aquabot_theboys/cmd_motors.py
@@ -10,64 +10,68 @@ from time import sleep
 class MyCmdMotors(Node):
     def __init__(self):
         super().__init__('cmd_motors') 
-        # Bot motor angle
+        # Angle des moteurs
         self.pub_pos = self.create_publisher(Float64, '/wamv/thrusters/main/pos', 10)
-        # thrust speed
+        # Vitesse des moteurs
         self.pub_thrust = self.create_publisher(Float64, '/wamv/thrusters/main/thrust', 10)
         
-        # Get position to reach
+        # Récupère la position à atteindre
         self.create_subscription(Float64MultiArray, '/position/to_reach', self.cmd_motors_callback, 10)
-        # Get actual orientation from the IMU
+        # Récupère l'orientation actuelle depuis l'IMU
         self.create_subscription(Float64, "/position/orientation", self.orientation_callback, 10)
-        # Get our actual position x,y
+        # Récupère notre position actuelle en x et y
         self.create_subscription(Float64MultiArray, "/position/current", self.position_actual, 10)
 
         self.create_subscription(Bool, '/position/look_around', self.look_around, 10)
 
         self.create_subscription(Float64MultiArray, '/position/follow_without_moving',self.follow_without_moving, 10)
 
-        # To know if we are changing its orientation
+        # Pour savoir si nous sommes en train de changer notre orientation
         self.aligning = False  
-        # x,y variable
         self.x_to_reach = 0.0
         self.y_to_reach = 0.0
         self.x_actual = 0.0
         self.y_actual = 0.0
-        # Our current orientation
         self.current_orientation = 0.0
 
-    # To get our actual x,y position
     def position_actual(self,msg):
+        """
+        Pour récupérer notre position actuelle en x et y.
+        """
         self.x_actual, self.y_actual = msg.data
 
-    # To get our actual orientation
     def orientation_callback(self,msg):
+        """
+        Fonction utilisée pour récupérer la valeur de l'orientation.
+        """
+
         self.current_orientation = msg.data
 
     def look_around(self,msg):
+        """
+        Fonction utilisée pour regarder si le bateau ennemi est autour de nous.
+        """
+
         msg_pos = Float64()
         msg_thrust = Float64()
-        #if (pre_orientation%(2*pi)) <= ((self.current_orientation+0.1)%(2*pi)) and (pre_orientation%(2*pi)) >= ((self.current_orientation-0.1)%(2*pi)):
         if msg.data == True:
             msg_pos.data = -pi/2
         else:
             msg_pos.data = pi/2
         msg_thrust.data = 200.0
-        # else:
-        #     msg_pos.data = 0.0
-        #     msg_thrust.data = 0.0
+
         self.pub_pos.publish(msg_pos)
         self.pub_thrust.publish(msg_thrust)
 
     def follow_without_moving(self, msg):
+        """
+        Fonction permettant de suivre du regard le bateau ennemi lorsqu'il apparait dans le champs de la caméra.
+        """
         msg_pos = Float64()
         msg_thrust = Float64()
         self.x_to_reach, self.y_to_reach = msg.data
         delta_x = (self.x_actual - self.x_to_reach) 
         delta_y = (self.y_actual - self.y_to_reach)
-        #self.get_logger().info('Delta x : %f, Delta y : %f   TWO' % (delta_x, delta_y))
-        #self.get_logger().info('Angle : %s' % str(atan2(delta_y, delta_x)))
-        # Calculating the angle we have to add at our orientation to point the object we want to reach
         angle_diff = ((atan2(delta_y, delta_x) + self.current_orientation) - pi) % (2*pi) 
         # A VOIR SI ON LAISSE
         if angle_diff > pi:
@@ -85,25 +89,23 @@ class MyCmdMotors(Node):
             msg_thrust.data = 500.0
             self.pub_thrust.publish(msg_thrust)
 
-    # To control motors with a x,y command
     def cmd_motors_callback(self, msg):
+        """
+        Fonction utilisée pour contrôler les moteurs en envoyant une position x et y.
+        """
         msg_pos = Float64()
         msg_thrust = Float64()
-        #self.get_logger().info('Current_pos : (%f;%f), pos to reach : (%f;%f)' % (self.x_actual, self.y_actual, self.x_to_reach, self.y_to_reach))
         self.x_to_reach, self.y_to_reach = msg.data
         delta_x = (self.x_actual - self.x_to_reach) 
         delta_y = (self.y_actual - self.y_to_reach)
-        #self.get_logger().info('Delta x : %f, Delta y : %f   TWO' % (delta_x, delta_y))
-        #self.get_logger().info('Angle : %s' % str(atan2(delta_y, delta_x)))
-        # Calculating the angle we have to add at our orientation to point the object we want to reach
+
         angle_diff = ((atan2(delta_y, delta_x) + self.current_orientation) - pi) % (2*pi) 
-        # A VOIR SI ON LAISSE
         if angle_diff > pi:
             angle_diff -= 2 * pi
         elif angle_diff < -pi:
             angle_diff += 2 * pi
-        #self.get_logger().info('Angle : %s' % str(angle_diff))
-        # If the position we want to reach is too near to our actual position, we stop the motors
+
+        # Si la position que nous voulons atteindre est trop proche de notre position réelle, nous arrêtons les moteurs.
         if ((self.x_to_reach <= self.x_actual +5) and (self.x_to_reach >= self.x_actual -5)) and ((self.y_to_reach <= self.y_actual +5) and (self.y_to_reach >= self.y_actual -5)):
             msg_pos.data = 0.0
             self.pub_pos.publish(msg_pos)
@@ -111,7 +113,7 @@ class MyCmdMotors(Node):
             self.pub_thrust.publish(msg_thrust)
 
         else :
-            # If the angle between us and the object we wand to reach is over 0.1 radians, we change our orientation
+            # Si l'angle entre nous et l'objet que nous voulons atteindre est supérieur à 0,1 radians, nous changeons d'orientation.
             if abs(angle_diff)>0.1: 
                 if angle_diff>0.0:
                     msg_pos.data = -pi/2
@@ -122,7 +124,7 @@ class MyCmdMotors(Node):
                 self.pub_thrust.publish(msg_thrust)
                 self.aligning = True
             else:
-                # If we were moving our boat before getting here, we stop them and wait until the position of the motors is not 0
+                # Si nous avons déplacé notre bateau avant d'arriver ici, nous l'arrêtons et attendons que la position des moteurs soit égale à 0.
                 if self.aligning  == True:
                     self.aligning  = False
                     msg_pos.data = 0.0
@@ -130,7 +132,6 @@ class MyCmdMotors(Node):
                     msg_thrust.data = 0.0
                     self.pub_thrust.publish(msg_thrust)
                     sleep(3)
-                    #self.get_logger().info('good')
                 else :  
                     msg_thrust.data = 10000.0
                     self.pub_thrust.publish(msg_thrust)
--- aquabot_theboys/object_detection_node.py
+++ aquabot_theboys/object_detection_node.py
@@ -15,24 +15,21 @@ import os
 
 
 class ObjectDetection:
-    """
-    Class implements Yolo5 model to make inferences on a video using OpenCV.
-    """
+
     
     def __init__(self):
 
         self.model = self.load_model()
         self.classes = self.model.names
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("\n\nDevice Used:",self.device)
 
 
 
     def load_model(self):
         """
-        Loads Yolo5 pretrained model.
-        :return: Trained Pytorch model.
+        Charge notre modèle pré-entrainé.
         """
+
         my_path = os.path.expanduser('~/vrx_ws/src/aquabot_theboys/resource/detectallobjects.pt')
         model = torch.hub.load('ultralytics/yolov5', 'custom', path=my_path,force_reload=True)
         return model
@@ -40,10 +37,9 @@ class ObjectDetection:
 
     def score_frame(self, frame):
         """
-        Takes a single frame as input, and scores the frame using yolo5 model.
-        :param frame: input frame in numpy/list/tuple format.
-        :return: Labels and Coordinates of objects detected by model in the frame.
+        Prend une seule image en entrée, et évalue l'image en utilisant le modèle.
         """
+
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
@@ -54,20 +50,17 @@ class ObjectDetection:
 
     def class_to_label(self, x):
         """
-        For a given label value, return corresponding string label.
-        :param x: numeric label
-        :return: corresponding string label
+        Retourne les différentes classes des objets.
         """
+
         return self.classes[int(x)]
 
 
     def plot_boxes(self, results, frame):
         """
-        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-        :param results: contains labels and coordinates predicted by model on the given frame.
-        :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
+        Cette fonction dessine des boites englobantes autour des objets détectés.
         """
+
         labels, cord = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
@@ -88,25 +81,32 @@ class ObjectDetectionNode:
     def __init__(self):
         self.node = rclpy.create_node('object_detection_node')
         self.detection = ObjectDetection()
+        #Interface entre ROS et OpenCV
         self.bridge = CvBridge()
-        self.current_orientation = 0.0  # Initial orientation
+        self.current_orientation = 0.0  
         self.object_detected_pub = self.node.create_publisher(Bool, 'object_detected', 10)
         self.object_position_pub = self.node.create_publisher(Float64, 'object_position', 10)
         self.node.create_subscription(Image, "/wamv/sensors/cameras/main_camera_sensor/image_raw", self.image_callback, 10)
         self.node.create_subscription(Imu, "/wamv/sensors/imu/imu/data", self.imu_callback, 10)
 
     def imu_callback(self, msg):
+        """
+        Transforme l'orientation de quaternion à euler.
+        """
         quaternion = (
             msg.orientation.w,
             msg.orientation.x,
             msg.orientation.y,
             msg.orientation.z
-        )
+        ) 
         _, _, self.current_orientation = quat2euler(qconjugate(quaternion))
 
-        #transform orientation from quaternion to euler
+       
 
     def image_callback(self, msg):
+        """
+        On rentre dans ce callback chaque fois qu'une image est reçue depuis la caméra.
+        """
         start_time = time.perf_counter()
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         results = self.detection.score_frame(frame)
@@ -120,32 +120,33 @@ class ObjectDetectionNode:
         is_detected_msg = Bool()
         x_position_msg = Float64()
 
-        # Convert classes dictionary to list
         classes_list = list(self.detection.classes.values())
 
-        # Check if any objects are detected
+        # Vérifie si un objet est détecté
         if len(results[0]) > 0:
             for i in range(len(results[0])):
-                # Check if the detected object is a 'red boat'
+                # Vérifie si l'objet est un 'red boat'
                 if int(results[0][i]) == classes_list.index('red boat'):
                     is_detected_msg.data = True
-                    # Calculate center of the box
+                    # Calcule le centre de la boite englobante
                     x1, y1, x2, y2 = results[1][i][:4]
                     x_center_norm = (x1 + x2) / 2.0
-                    # Convert in pixels
+                    # Converti en pixels
                     x_center_pixel = x_center_norm * frame.shape[1]
-                    # Calculate difference from the frame center
+                    # Calcule la différence depuis le centre de l'image
                     diff = x_center_pixel - frame.shape[1] / 2
-                    # Convert difference to angle
+                    # Converti la différence en angle
                     angle = atan(diff / frame.shape[1])
                     x_position_msg.data = angle  
                     break
             else:
                 is_detected_msg.data = False
-                x_position_msg.data = 0.0  # No angle to calculate
+                # Pas d'angle à calculer
+                x_position_msg.data = 0.0  
         else:
             is_detected_msg.data = False
-            x_position_msg.data = 0.0  # No angle to calculate
+            # Pas d'angle à calculer
+            x_position_msg.data = 0.0  
 
         self.object_detected_pub.publish(is_detected_msg)
         self.object_position_pub.publish(x_position_msg)
@@ -159,7 +160,6 @@ node = ObjectDetectionNode()
 
 rclpy.spin(node.node)
 
-# Free ressources
 cv2.destroyAllWindows()
 node.node.destroy_node()
 rclpy.shutdown()
